d844427165f7023b84e5f08fb90d82a4ae37b94f
Prateek <prateekjain259@gmail.com>
2018-04-16 11:49:41 +0530

    Updating Repository 16/04/2018 * Fixed the bugs in ScoreBoard

--- app/src/main/java/com/example/prateek/testmp/HomeStudentActivity.java
+++ app/src/main/java/com/example/prateek/testmp/HomeStudentActivity.java
@@ -4,12 +4,16 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -104,4 +108,43 @@ public class HomeStudentActivity extends AppCompatActivity {
         });
     }
 
+
+    //menu code
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_testing, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        switch (item.getItemId()) {
+
+            case R.id.profile:
+                //setLanguage("English");
+                //Log.i("Selected","English");
+
+                return true;
+            case R.id.logout:
+                //setLanguage("Hindi");
+                FirebaseAuth.getInstance().signOut();
+                Intent intent=new Intent(this,MainActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        super.onBackPressed();
+    }
 }
---
+++ app/src/main/java/com/example/prateek/testmp/ScoreBoard.java
@@ -0,0 +1,20 @@
+package com.example.prateek.testmp;
+
+/**
+ * Created by Vaidik on 4/8/2018.
+ */
+
+public class ScoreBoard {
+
+    public String name;
+    public String marks;
+
+    public ScoreBoard(){
+
+    }
+
+    public ScoreBoard(String name, String marks) {
+        this.name = name;
+        this.marks = marks;
+    }
+}
